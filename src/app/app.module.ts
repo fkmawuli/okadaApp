549d79ad0070ee1ffd1823767eefe03b1cbb139f
Fredercik Mawuli <fkmawuli@gmail.com>
2017-06-22 11:31:00 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+
+export const firebaseConfig ={
+    apiKey: "AIzaSyCVJe6LAQgio5j-t7xvN-qj0krJwK8NRek",
+    authDomain: "motolog-1d021.firebaseapp.com",
+    databaseURL: "https://motolog-1d021.firebaseio.com",
+    projectId: "motolog-1d021",
+    storageBucket: "motolog-1d021.appspot.com",
+    messagingSenderId: "64584749390"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+// import {AngularFire, AuthProviders, AuthMethods} from "angularfire2/angularfire2";
+import {Router} from '@angular/router';
+// import {moveIn} from '../router.animations';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  // animations: [moveIn()],
+  host:{'[@moveIn]':''}
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
